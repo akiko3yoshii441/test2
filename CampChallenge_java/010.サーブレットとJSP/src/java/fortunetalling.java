6531d17a36d7afe4584522d3f600ba6db64a3db6
akiko3yoshii441 <akiko3yoshii@gmail.com>
2017-12-26 14:31:46 +0900

    サーブレットとJSP　課題提出します

---
+++ CampChallenge_java/010.サーブレットとJSP/src/java/fortunetalling.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Date;
+import java.util.Random;
+import javax.servlet.RequestDispatcher;
+import org.camp.servlet.ResultData;
+
+/**
+ *
+ * @author yoshiiakiko
+ */
+@WebServlet(urlPatterns = {"/TestByArai"})
+public class fortunetalling extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            final String result = "/WEB-INF/FortuneTellingResult.jsp";
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet TestByArai</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.print("<h2>Fortune Telling Yoshii at " + request.getContextPath() + "</h2>");
+            out.println("<h3>芳井うらない<h3>");
+            out.println("<h1>おめでとう！<h1>");
+            out.println("今日のあなたの運勢です！");
+
+            String luckList[] = {"大吉", "中吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "凶", "大凶"};
+
+            Random rand = new Random();
+
+            Integer index = rand.nextInt(luckList.length);
+            out.println(luckList[index]);
+
+            out.println("</body>");
+            out.println("</html>");
+
+            ResultData data = new ResultData();
+            data.setD(new Date());
+            data.setLuck(luckList[index]);
+            request.setAttribute("DATA", data);
+
+            RequestDispatcher rd = request.getRequestDispatcher(result);
+            rd.forward(request, response);
+
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
