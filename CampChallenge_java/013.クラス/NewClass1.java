236679ab9327da8dddf0a86ca5dad3f119902d7d
akiko3yoshii441 <akiko3yoshii@gmail.com>
2018-01-13 11:20:22 +0900

    クラス　課題１　提出します

---
+++ CampChallenge_java/013.クラス/NewClass1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yoshiiakiko
+ */
+//以下の機能を持つクラスを作成してください。
+//1. パブリックな２つの変数と、２つの変数に値を設定するパブリックなメソッドと、２つの変数の中身をprintするパブリックなメソッド
+
+public class NewClass1 {
+    public static void main(String[] args) {
+    String name = fullName("akiko","jones");
+    
+    System.out.println(name);
+            
+  
+    }
+    
+    public static String fullName(String firstName, String lastName) {
+    return firstName + "" + lastName;
+    }
+}
+
+
