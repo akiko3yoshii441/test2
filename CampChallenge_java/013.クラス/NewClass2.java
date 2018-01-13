54ee927a973e805d3b289be4b2e6fffa6edb7ee0
akiko3yoshii441 <akiko3yoshii@gmail.com>
2018-01-13 11:21:14 +0900

    クラス　課題２　提出します

---
+++ CampChallenge_java/013.クラス/NewClass2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yoshiiakiko
+ */
+public class NewClass2 {
+    public static void main(String[] args) {
+        
+        
+    
+    
+    class kara extends NewClass1 {
+    public void nakamiKara() {
+    String name = fullName("","");
+    
+   System.out.println(name);
+    }
+  }    
+    
+} 
+    
+    
+    
+    
+    
+        
+    
+    
+   
+      
+}
+
+    
+
