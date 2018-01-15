7d00570b919c225fc4fcd4436dece22c6a00ca01
akiko3yoshii441 <akiko3yoshii@gmail.com>
2018-01-15 17:10:32 +0900

    メソッド　引数と戻り値　課題１　提出します

---
+++ CampChallenge_java/011.メソッド/hikisuutomodorichi1:3.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author yoshiiakiko
+ */
+//引数が１つのメソッドを作成してください。
+//メソッドの中の処理は、３人分のプロフィール（項目は戻り値2と同様）を作成し、引数として渡された値と同じIDを持つ人物のプロフィールを返却する様にしてください。
+//それ以降は課題「戻り値2」と同じ処理にしてください。
+public class hikisuutomodorichi1 extends HttpServlet {
+
+
+    String[] ourData(int num) {
+        //プロフィール３個作る
+        String[] aData = {"1", "aki", "19911202", "nippori"};
+        String[] bData = {"2", "ifi", "19960224", "nippori"};
+        String[] cData = {"3", "kana", "19960221", "titosehunabashi"};
+
+        if (num == 1) {return aData;
+        } else if (num == 2) {return bData;
+        } else {return cData;
+        }
+
+        //受け取る引数で表示するプロフィール異なる
+        
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter pw = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String[] result = ourData(2);
+
+            //プロフィールを表示する
+            pw.println(result[1]);
+            pw.println(result[2]);
+            pw.println(result[3]);
+//プロフィール表示o
+        }
+
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
